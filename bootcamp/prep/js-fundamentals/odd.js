5f25a72ba4eb6e2f0c2e0250d28778ae7e0f80e4
Chandani Kumari <16663673+chandanikumari@users.noreply.github.com>
2021-10-13 19:29:47 -0700

    added odd & voting-station files

---
+++ bootcamp/prep/js-fundamentals/odd.js
@@ -0,0 +1,11 @@
+function isOdd(num){
+  if(num % 2 !== 0){
+      return true;
+  }
+  else{
+    return false;
+  }
+}
+
+console.log("7 is odd: " + isOdd(7));
+console.log("10 is odd: " + isOdd(10));
